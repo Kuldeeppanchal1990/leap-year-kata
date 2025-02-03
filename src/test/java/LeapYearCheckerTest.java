08a26e2013d152b5ed0f1d6b8b2cafe86a15fb2e
Kuldeep Panchal <kuldeepsubscription@gmail.com>
2025-02-04 00:14:17 +0530

    Refactoring

--- src/test/java/LeapYearCheckerTest.java
+++ src/test/java/LeapYearCheckerTest.java
@@ -1,18 +1,27 @@
 import org.incubyte.LeapYearChecker;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearCheckerTest {
 
+    private LeapYearChecker leapYearChecker;
+
+    @BeforeEach
+    public void beforeEach() {
+        leapYearChecker = new LeapYearChecker();
+    }
+
     @Test
     public void notALeapYear_WhenYearNotDivisibleBy4() {
-        LeapYearChecker leapYearChecker = new LeapYearChecker();
-        Assertions.assertFalse(leapYearChecker.check(1997));
+        assertFalse(leapYearChecker.check(1997));
     }
 
     @Test
     public void isALeapYear_WhenDivisibleBy4() {
-        LeapYearChecker leapYearChecker = new LeapYearChecker();
-        Assertions.assertTrue(leapYearChecker.check(1996));
+        assertTrue(leapYearChecker.check(1996));
     }
 }
